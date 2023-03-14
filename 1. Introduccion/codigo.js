c44059c0c439fd69f12bc8efcef0e6bbe839a078
javierportillar <javierandres.008@hotmail.com>
2023-03-14 16:46:15 -0500

    primeros cods con js

--- 1. Introduccion/codigo.js
+++ 1. Introduccion/codigo.js
@@ -2,4 +2,12 @@ let numero = 23, numero2=25;
 
 let numero3= 15;
 let numero4= 29;
-alert(numero)
+resultado = "" + numero3 + numero4;
+andres = `<h1>Hola</h1>`;
+alert(andres)
+
+saludo = "Hola ";
+pregunta ="como vas";
+
+frase = saludo + pregunta;
+
