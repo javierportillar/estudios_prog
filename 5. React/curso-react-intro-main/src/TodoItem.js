f30c56e66532daf7a27ddcda91d29c61130f750f
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-07-27 17:53:02 -0500

    Funciona normal, se agregará los diferentes botones

--- 5. React/curso-react-intro-main/src/TodoItem.js
+++ 5. React/curso-react-intro-main/src/TodoItem.js
@@ -1,12 +1,11 @@
 import './TodoItem.css'
+import { DeleteIcon } from "./DeleteIcon";
+import { CompleteIcon } from "./CompleteIcon";
 
 function TodoItem(props){
   return(
     <li className="TodoItem">
-      <span className={
-        `Icon Icon-check ${props.completed && "Icon-check--active"}`
-        }
-        onClick={props.onComplete}>V</span>
+      <CompleteIcon/>
       <p className={`TodoItem-p ${props.completed && "TodoItem-p-completed"} `}>{props.text}</p>
       <span className="Icon Icon-delete">X</span>
     </li>
