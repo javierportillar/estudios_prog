6bc57850e059928edaa433caaef2c9ce97a3baac
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-08-09 12:30:59 -0500

    Se agregó directivas, estas permiten hacer modificaciones al dom, hay que conocer bien las directivas para diferentes funcionalidades

--- 3. FrameWorks/2. Angular/curso-angular/src/app/app.module.ts
+++ 3. FrameWorks/2. Angular/curso-angular/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ImgComponent } from './components/img/img.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
 import { NavComponent } from './components/nav/nav.component';
+import { ReversePipe } from './pipes/reverse.pipe';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,8 @@ import { NavComponent } from './components/nav/nav.component';
     ProductComponent,
     ProductsComponent,
     NavComponent,
+    ReversePipe,
+    HighlightDirective,
   
   ],
   imports: [
