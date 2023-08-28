3fd2423d8f306d51af9e4d68b37e7ad13476ffd5
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-08-28 06:41:57 -0500

    Vimos erroes con try y catch, ahora veremos procesos hijo

---
+++ 4. Backend/Node_js/3_modules/errores-try-catch.js
@@ -0,0 +1,25 @@
+async function errorAsync(){
+    setTimeout(()=>{
+        try{
+            return 3+z;
+        }catch(e){
+            console.error('Mira wuacho sumas numero y letras');
+            // console.error(e);
+        }
+    },1500)
+}
+
+ function posibleError(){
+    try{
+        return 3+z;
+    }catch(e){
+        console.error('Mira wuacho sumas numero y letras');
+        // console.error(e);
+    }
+}
+
+
+try{errorAsync()
+} catch(errorrrrr){
+    console.error('errorrrrr')
+}
